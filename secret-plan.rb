ef12addd57ba17480206ee1d4987bd2a28715e0d
goreleaserbot <goreleaser@carlosbecker.com>
2022-10-02 16:05:38 +0900

    Brew formula update for secret-plan version v0.0.32

--- secret-plan.rb
+++ secret-plan.rb
@@ -5,20 +5,20 @@
 class SecretPlan < Formula
   desc "For secret value diff and save tool.(e.g aws secretsmanager)"
   homepage "https://github.com/yutachaos/secret-plan"
-  version "0.0.31"
+  version "0.0.32"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yutachaos/secret-plan/releases/download/v0.0.31/secret-plan_0.0.31_darwin_arm64.zip"
-      sha256 "6cd4e13fff361160cc6f5146e3361a9da676908ed4769da072c86e8630d203a9"
+      url "https://github.com/yutachaos/secret-plan/releases/download/v0.0.32/secret-plan_0.0.32_darwin_arm64.zip"
+      sha256 "481ceaef8841e2c68dc130b6fba6dad116aeb954f29576ce75edfcdb54bf2ba8"
 
       def install
         bin.install Dir['secret-plan']
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/yutachaos/secret-plan/releases/download/v0.0.31/secret-plan_0.0.31_darwin_amd64.zip"
-      sha256 "b3a7c0703ae4c1eb6b7fbe11ecb3897ef61773e98d6060e919321a40ca132896"
+      url "https://github.com/yutachaos/secret-plan/releases/download/v0.0.32/secret-plan_0.0.32_darwin_amd64.zip"
+      sha256 "595dceb56583d3ca605d79064d7580002fae5fd9fe412b81b8419314b86b4530"
 
       def install
         bin.install Dir['secret-plan']
@@ -28,16 +28,16 @@ class SecretPlan < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/yutachaos/secret-plan/releases/download/v0.0.31/secret-plan_0.0.31_linux_arm64.zip"
-      sha256 "464dbfaadf897faebbfe1774363da0bd5a4706d9dafbd00e3acc2ef01ecd4d20"
+      url "https://github.com/yutachaos/secret-plan/releases/download/v0.0.32/secret-plan_0.0.32_linux_arm64.zip"
+      sha256 "44b560e1c6948ea7d1dc71e933cc7b0b44fd441b5bdcc892d695754a086c7e12"
 
       def install
         bin.install Dir['secret-plan']
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/yutachaos/secret-plan/releases/download/v0.0.31/secret-plan_0.0.31_linux_amd64.zip"
-      sha256 "d4b21493f5ba7d25ffa0468da43beb954f4872a326af55e10d8ae2951bb8d8b2"
+      url "https://github.com/yutachaos/secret-plan/releases/download/v0.0.32/secret-plan_0.0.32_linux_amd64.zip"
+      sha256 "724ba3aced2a84c78bb12f9782b109796bbc5e25043e47d1effb06d5dc3ccf45"
 
       def install
         bin.install Dir['secret-plan']
